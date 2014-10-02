af8f08774dd92bcfd76b572c7829b0daa8b3fe2b
Meme <man.m25@gmail.com>
2014-10-02 17:05:19 -0500

    arreglando falla 2

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,7 +3,7 @@ class Photo < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :product
-  belongs_to :imageable, polymorphic: true
+  belongs_to :photos, polymorphic: true
 
   has_attached_file :photo, :styles => {:biggest => "900x900>", :big => "600x600>", :medium => "400x400>", :small => "200x200>", :smallest =>"100x100>" },
                     :url  => ":s3_domain_url",
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
 
-  has_many :images, as :imageable
+  has_many :photos, as :imageable
   belongs_to :user
   belongs_to :subcategory
 
